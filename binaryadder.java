3a08f0637e0cc79764f05710e261a5f300644093
David Stevens <teamjtf4@gmail.com>
2021-09-09 15:04:02 -0400

    bug fixed and performance improvements

--- binaryadder.java
+++ binaryadder.java
@@ -8,27 +8,28 @@ public class binaryadder {
 		@SuppressWarnings("resource") //Java kept yelling at me for a resource leak.. so I added this suppression.
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("\nProvide two 4-bit binary values, in two separate inputs.\nThey will be added and converted into decimal!\n");
-		int a1 = 0;
+
+		int entry1, entry2 = 0; //Define the needed strings for the calculation.
+		int a, b = 0; //Define all of the needed integers for the calculation.
 		
-		for(String entry1 = keyboard.next(); a1 >= 0; entry1 = keyboard.next()) {
+		for(entry1 = keyboard.nextInt(); entry1 >= 0; entry1 = keyboard.nextInt()) {
 			System.out.print(" +\n"); //The for statement begins by prompting the user to enter a value, which gets stored as entry1 (this is condition1). Every time the for statement is run (each cycle), the for statement will run condition3, which too prompts the user to enter a value (to be stored as entry1). The values of previous cycles can be disregarded, so they are simply overwritten with each cycle.
 			
-			String entry2 = keyboard.next();
+			entry2 = keyboard.nextInt();
 		
-				a1 = Integer.parseInt(entry1.charAt(0)+"")*2*2*2; //This block of code turns the String value of the first entry into integers that can then be translated into binary. The stacking of multiples of two is the binary conversion.
-				int a2 = Integer.parseInt(entry1.charAt(1)+"")*2*2;
-				int a3 = Integer.parseInt(entry1.charAt(2)+"")*2;
-				int a4 = Integer.parseInt(entry1.charAt(3)+"");
-		
-				int b1 = Integer.parseInt(entry2.charAt(0)+"")*2*2*2; //This block of code turns the String value of the second entry into integers that can then be translated into binary. The stacking of multiples of two is the binary conversion.
-				int b2 = Integer.parseInt(entry2.charAt(1)+"")*2*2;
-				int b3 = Integer.parseInt(entry2.charAt(2)+"")*2;
-				int b4 = Integer.parseInt(entry2.charAt(3)+"");
+			a = calculator(entry1); //Define binary integer A
+			b = calculator(entry2); //Define binary integer B
 		
 			System.out.println(" ="); //This is the beginning of a line of System output. Since the math would be incorrect when text was between the integers, the following are .print(). and not .println().
-			System.out.print(a1+a2+a3+a4+" + ");
-			System.out.print(b1+b2+b3+b4+" in decimal\n =\n");
-			System.out.print(a1+a2+a3+a4+b1+b2+b3+b4+" in decimal\n\nEnter two 4-bit binary values again to continue calculating.\n\n"); //This is the output of the value, which is calculated by adding all of the integers a1-4, and b1-4 together.
+			System.out.print(a + " + ");
+			System.out.print(b + " in decimal\n =\n");
+			System.out.print(a +b + " in decimal\n\nEnter two binary values again to continue calculating.\n\n"); //This is the output of the value, which is calculated by adding all of the integers a1-4, and b1-4 together.
 		}
+	}
+
+	private static int calculator(int number) { //This function converts the provided integer to binary
+		String numString = Integer.toString(number);
+		int result = Integer.parseInt(numString,2);
+		return result;
 	}
 }
